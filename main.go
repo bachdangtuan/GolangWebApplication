ce975cdf1b5886efb641cb4f459b660df6f4c0ea
DESKTOP-U588U2V\admin <bachdangtuan93@gmail.com>
2023-08-18 02:18:55 +0700

    no message

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
     "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	
 )
 
